6961684b950fdc6fc072b1f41827830425e49fff
thcl_hkr <kayshcache@github.com>
2020-03-10 12:52:10 +1100

    Moving - insignificant commit

--- react-client/src/components/CashFloatForm.js
+++ react-client/src/components/CashFloatForm.js
@@ -1,32 +1,62 @@
 import React from 'react';
+import axios from 'axios';
+
+// TODO: learn React-axios
 
 export default class CashFloatForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      cashFloat: {},
+    };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
+  
+  componentDidMount() {
+    axios.get('/cash')
+      .then(res => {
+	this.setState({
+	  cashFloat: res.data[0],
+	  cashFloatId: res.data[0]._id,
+	});
+	console.dir(this.state);
+      })
+      .catch(err => console.warn(err));
+  }
 
   handleChange(event) {
-    this.setState({fiveCents: event.target.value});
+    this.setState({cashFloat: {
+      fiveCents: event.target.value}
+    });
   }
 
   handleSubmit(event) {
-    console.log(this.state.fiveCents);
+    console.log(this.state);
+    axios.put(`/cash/${this.state.cashFloatId}`, {tenCents: event.target.value})
+      .then(res => console.log(res.data))
+      .catch();
     event.preventDefault();
   }
 
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
+      <p>
         <label>
           Five Cents:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
+          <input type="text" value={this.state.cashFloat.fiveCents} onChange={this.handleChange} />
         </label>
+      </p>
+      <p>
+        <label>
+          Ten Cents:
+          <input type="text" value={this.state.cashFloat.tenCents} onChange={this.handleChange} />
+        </label>
+      </p>
         <input type="submit" value="Submit" />
-	<p><textarea value={this.state.fiveCents} onChange={this.handleChange} /></p>
+	<p><textarea value={this.state.cashFloat.fiveCents} onChange={this.handleChange} /></p>
       </form>
     );
   }
